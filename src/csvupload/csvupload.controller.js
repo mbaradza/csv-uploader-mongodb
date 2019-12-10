04ff0b636b1a2207bb2c0cfb3ecbc7f1e323e491
mbaradza <mbaradza@hitrac.co.zw>
2019-12-10 06:42:19 +0200

    Handle duplicate columns

--- src/csvupload/csvupload.controller.js
+++ src/csvupload/csvupload.controller.js
@@ -1,36 +1,55 @@
 const mongodb = require("mongodb").MongoClient;
 const csvtojson = require("csvtojson");
 var config = require('../config');
+var fs = require('fs');
 
 exports.upload = async (req, res) => {
 
-   const database = req.fields.database;
-  const collection =req.fields.collection;
+  const database = req.fields.database;
+  const collection = req.fields.collection;
   const filePath = req.files.file.path
   const url = config.mongo.url
-  
-   csvtojson()
-  .fromFile(filePath)
-  .then(csvData => {
-    console.log(csvData);
 
-    mongodb.connect(
-      url,
-      { useNewUrlParser: true, useUnifiedTopology: true },
-      (err, client) => {
-        if (err) throw err;
+  var newHeaders = []
 
-        client
-          .db(database)
-          .collection(collection)
-          .insertMany(csvData, (err, res) => {
-            if (err) throw err;
-            console.log(`Inserted: ${res.insertedCount} rows`);
-            client.close();
-          });
+  //Read Headers Only And Manipulate Them
+  await csvtojson()
+    .on('header', (header) => {
+      for (var h of header) {
+        if (newHeaders.indexOf(h) >= 0) {
+          h = h + "_1"
+        }
+        newHeaders.push(h)
       }
-    );
-    res.status(201).json(csvData);
-  })  
+    })
+    .fromFile(filePath);
+
+  //Second Call To Read The Data And Attach The New Headers
+  csvtojson({
+    noheader: false,
+    headers: newHeaders
+  })
+    .fromFile(filePath)
+    .then(csvData => {
+      console.log(csvData);
+
+      mongodb.connect(
+        url,
+        { useNewUrlParser: true, useUnifiedTopology: true },
+        (err, client) => {
+          if (err) throw err;
+
+          client
+            .db(database)
+            .collection(collection)
+            .insertMany(csvData, (err, res) => {
+              if (err) throw err;
+              console.log(`Inserted: ${res.insertedCount} rows`);
+              client.close();
+            });
+        }
+      );
+      res.status(201).json(csvData);
+    })
 
 }
